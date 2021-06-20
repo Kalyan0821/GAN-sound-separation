fix softmax constraint

train.py
@@ -192,23 +192,18 @@ disc_optimizer = create_optimizer(nets=nets, mode="disc", opt=opt)
 # Loss functions
 classification_loss = nn.BCEWithLogitsLoss()
 
-if opt.softmax_constraint:
-	regression_loss = None  # Impose SoftMax constraint to make masks sum to 1
-else:
-	if opt.mask_loss_type == "L1":
-		regression_loss = nn.L1Loss()
-	elif opt.mask_loss_type == "L2":
-		regression_loss = nn.MSELoss()
-	elif opt.mask_loss_type == "BCE":
-		regression_loss = nn.BCELoss()
-
+if opt.mask_loss_type == "L1":
+	regression_loss = nn.L1Loss()
+elif opt.mask_loss_type == "L2":
+	regression_loss = nn.MSELoss()
+elif opt.mask_loss_type == "BCE":
+	regression_loss = nn.BCELoss()
 
 disc_losses = []
 real_confs = []
 fake_confs = []
 gen_losses_consistency = []
 gen_losses_classification = []
-
 
 # Train
 batch_number = 0

utils/train_utils.py
@@ -24,26 +24,31 @@ def softmax_normalization(raw_predicted_masks, clip_ids):
 		clip_ids: B """
 
 	raw_predicted_mask_aggregates = dict()
-	normalized_predicted_mask_aggregates = dict()
 	predicted_masks_list = []
 
 	B, C, F, T = raw_predicted_masks.shape
 
 	for clip_id in clip_ids:
 		raw_predicted_mask_aggregates[clip_id.item()] = []
-		normalized_predicted_mask_aggregates[clip_id.item()] = [] 
 
 	for i, clip_id in enumerate(clip_ids):
 		raw_predicted_mask_aggregates[clip_id.item()].append(raw_predicted_masks[i:(i+1)])  # [list of 1xCxFxT tensors]
 
-	for i, clip_id in enumerate(clip_ids):
-		normalized_predicted_mask_aggregates[clip_id.item()] = nn.Softmax(
-															   torch.stack(raw_predicted_mask_aggregates[clip_id.item()], dim=0),
-															   dim=0)  # k x C x F x T
-		predicted_masks_list.append(normalized_predicted_mask_aggregates)
+	softmax = nn.Softmax(dim=0)
+	prev_clip_id = -1
+	for clip_id in clip_ids:
+		if clip_id == prev_clip_id:
+			continue
 
-	predicted_masks = torch.stack(predicted_masks_list, dim=0)
-	assert predicted_masks.shape == raw_predicted_masks.shape
+		predicted_masks_list.append(softmax(
+									torch.cat(raw_predicted_mask_aggregates[clip_id.item()], dim=0)))  # k x C x F x T
+
+		prev_clip_id = clip_id 
+
+	predicted_masks = torch.cat(predicted_masks_list, dim=0)
+	assert predicted_masks.shape == raw_predicted_masks.shape, f"{predicted_masks.shape, raw_predicted_masks.shape}"
 
 	return predicted_masks  # B x 1 x F x T
+
+
 